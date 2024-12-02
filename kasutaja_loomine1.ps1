2bd7cd49240d45649ca60e342f81ff5cc1ce354b
Sander Salu <sander.salu.002@voco.ee>
2024-12-02 09:27:09 +0200

    Creating local user with command line parameters

---
+++ kasutaja_loomine1.ps1
@@ -0,0 +1,10 @@
+﻿if($args.Count -ne 3) {
+    Write-Host -ForegroundColor Red '.\script username "Full Name" "Account description"'
+} else {
+    $username = $args[0]
+    $fullname = $args[1]
+    $description = $args[2]
+
+    $userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+    New-LocalUser 'kasutaja1' -Password $userPassword -FullName 'Esimene kasutaja' -Description 'Local Acc - kasutaja1'
+}
